45a5c3aeafeda033dcc987ebcd8680f16cfbdce4
hankliu <397694072@qq.com>
2024-04-30 08:17:47 +0800

    feat(lifecount): add config files

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+const fabric = require('@hankliu/fabric');
+
+module.exports = [
+  {...fabric.eslint, files: ["src/**/*.ts", "src/**/*.tsx", "src/**/*.js"],},
+    {
+      ignores: ["public/*", "out/*", "docs/*"],
+    }
+];
