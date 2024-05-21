3a990ef695987426cbc96e7a49edb8995b77d766
hankliu <397694072@qq.com>
2024-05-21 09:12:07 +0800

    feat(lifecount): add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,26 @@
+export const PageTitle = 'H.L Toolkits - 人生一格';
+
+export const PageSubTitle = [
+  `人生是一条无止境的路，你永远不知道自己会遇到什么样的困难，但是你要相信，只要你勇敢地走过去，剩余的问题就显得微不足道了。`,
+  '人生那就是一场永不落幕的演出，我们每一个人都是演员，只不过，有的人顺从自己，有的人取悦观众。',
+];
+
+export const PageDescription = `人生进度条，人生是一条无止境的路，你永远不知道自己会遇到什么样的困难，但是你要相信，只要你勇敢地走过去，剩余的问题就显得微不足道了。`;
+
+export const PageKeywords = [
+  '人生一格',
+  '人生格式化',
+  '人生小格',
+  'lifecount',
+  'toolkit',
+  'toolkits',
+  '前端开发',
+  '前端开发工具',
+  '前端开发工具集合',
+  '在线工具',
+  'toolbox',
+  'frontend',
+  '卡鲁秋',
+  'Hank',
+  'HankLiu',
+];
